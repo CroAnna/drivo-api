a9b843ad57d151856322d9890e18330e82face73
Ana Škarica <ana.skarica.as@gmail.com>
2025-03-17 20:08:26 +0100

    Remove tests, add role based access to stats

--- src/main/java/org/croanna/resources/StatsResource.java
+++ src/main/java/org/croanna/resources/StatsResource.java
@@ -1,5 +1,7 @@
 package org.croanna.resources;
 
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -15,7 +17,7 @@ import org.croanna.services.StatsService;
 import java.util.Collections;
 import java.util.List;
 
-//@PermitAll
+@PermitAll
 @Path("/api/stats")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -26,7 +28,7 @@ public class StatsResource {
 
     @GET
     @Path("/active-drivers-per-category")
-//    @RolesAllowed({"ADMIN", "EMPLOYEE", "INSTRUCTOR"})
+    @RolesAllowed({"ADMIN", "EMPLOYEE", "INSTRUCTOR"})
     public Response getActiveDriversPerCategory() {
         DatasetResponseDTO datasetResponse = statsService.getActiveDriversPerCategory();
         ChartDatasetResponseDTO chartDataset = new ChartDatasetResponseDTO("Number of drivers per category", datasetResponse.getData());
@@ -36,7 +38,7 @@ public class StatsResource {
 
     @GET
     @Path("/instructors-per-category")
-//    @RolesAllowed({"ADMIN", "EMPLOYEE", "INSTRUCTOR"})
+    @RolesAllowed({"ADMIN", "EMPLOYEE", "INSTRUCTOR"})
     public Response getInstructorsPerCategory() {
         DatasetResponseDTO datasetResponse = statsService.getInstructorsPerCategory();
         ChartDatasetResponseDTO chartDataset = new ChartDatasetResponseDTO("Number of instructors per category", datasetResponse.getData());
